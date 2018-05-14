424e863292320b74e44f608d30035f09434966e0
yiphei <yanyiphei@gmail.com>
2018-05-14 10:05:06 -0400

    m

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -16,6 +16,7 @@ export const signin = (req, res, next) => {
   res.send({ token: tokenForUser(req.user) });
 };
 
+
 export const signup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
